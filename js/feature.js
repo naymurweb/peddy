7f409a52d86f557fcd34d9c3ddd6cdc005be5d51
naymurweb <naymurweb@gmail.com>
2024-12-29 21:46:53 +0600

    pet details data load and show

--- js/feature.js
+++ js/feature.js
@@ -40,31 +40,28 @@ const petsData = () => {
 };
 
 const showPets = (data) => {
-    const petContainer = document.getElementById("pet-container");
+  const petContainer = document.getElementById("pet-container");
 
-    if(data.length===0){
-        petContainer.classList.remove('grid')
-        petContainer.classList.add('w-full')
-      
-        petContainer.innerHTML=`
+  if (data.length === 0) {
+    petContainer.classList.remove("grid");
+    petContainer.classList.add("w-full");
+
+    petContainer.innerHTML = `
        <div class="w-full flex flex-col justify-center text-center items-center bg-[#13131308] rounded-lg p-10">
         <img src='../images/error.webp' width="400px"/>
         <h1 class='text-4xl font-bold my-4' >No Information Available</h1>
         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at </br>
 its layout. The point of using Lorem Ipsum is that it has a.</p>
        </div>
-        `
-        
-
-    }
-    else{
-        petContainer.innerHTML = "";
-        petContainer.classList.add('grid')
-        petContainer.classList.add('w-full')
-        data.forEach((pet) => {
-          const item = document.createElement("div");
-          item.classList = "card card-compact bg-base-100  shadow-xl border pt-5";
-          item.innerHTML = `
+        `;
+  } else {
+    petContainer.innerHTML = "";
+    petContainer.classList.add("grid");
+    petContainer.classList.add("w-full");
+    data.forEach((pet) => {
+      const item = document.createElement("div");
+      item.classList = "card card-compact bg-base-100  shadow-xl border pt-5";
+      item.innerHTML = `
                 <figure >
                     <img
                     class="rounded-lg"
@@ -90,13 +87,54 @@ its layout. The point of using Lorem Ipsum is that it has a.</p>
                     <div class="flex justify-between border-t-2 py-3 items-center">
                       <button class="btn btn-sm text-primary text-xl px-8"><i class="fa-regular fa-thumbs-up"></i></button>
                       <button class="btn btn-sm text-primary text-lg">Adopt</button>
-                      <button class="btn btn-sm text-primary text-lg">Details</button>
+                      <button onclick={petDetails(${
+                        pet?.petId
+                      })}  class="btn btn-sm text-primary text-lg" >Details</button>
                     </div>
               `;
-          petContainer.append(item);
-        });
-
-    }
-
+      petContainer.append(item);
+    });
+  }
 };
+
+const petDetails = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/peddy/pet/${id}`)
+    .then((res) => res.json())
+    .then((data) => showPetDetails(data.petData));
+};
+
+const showPetDetails = (data) => {
+  const model = document.getElementById("modal");
+  model.click();
+  const modalContent = document.getElementById("modal-content");
+  modalContent.innerHTML = `
+    <div class='w-full'>
+    <img src=${data.image} width="100%"/>
+    </div>
+    
+  <div class="card-body text-gray-500">
+                    <h2 class="card-title text-black">${
+                      data?.pet_name ? data?.pet_name : "Not Available"
+                    }</h2>
+                    <p><i class="fa-brands fa-squarespace"></i> Breed: ${
+                      data?.breed ? data?.breed : "Not Available"
+                    }</}</p>
+                    <p><i class="fa-solid fa-calendar-days"></i> Birth: ${
+                      data?.date_of_birth ? data.date_of_birth : "Not Available"
+                    }</p>
+                    <p><i class="fa-solid fa-mercury"></i> Gender: ${
+                      data?.gender ? data?.gender : "Not Available"
+                    }</p>
+                    <p><i class="fa-solid fa-mercury"></i> Price : ${
+                      data?.price ? data?.price : "Not Available"
+                    }</p>
+                    <hr />
+                    <h3 class="text-2xl font-bold">
+                    Details Information
+                    <h3>
+                    <p>${data.pet_details}</p>
+
+    `;
+};
+
 petsData();

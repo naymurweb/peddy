c01bf0d9cd9b162461b2f5c6817fc45b83b776c0
naymurweb <naymurweb@gmail.com>
2024-12-29 20:38:54 +0600

    error page show

--- js/feature.js
+++ js/feature.js
@@ -7,25 +7,26 @@ const categories = () => {
 
 const showCategory = (data) => {
   const categoriesContainer = document.getElementById("categories-container");
-
   data.forEach((pet) => {
     const btn = document.createElement("button");
-    btn.classList.add(
-      "btn",
-      "btn-outline",
-      "btn-xs",
-      "sm:btn-sm",
-      "md:btn-md",
-      "lg:btn-lg",
-      "px-16",
-      "border-gray-300",
-      "hover:text-black",
-      "hover:bg-[#0E7A811A]",
-      "hover:border-primary"
-    );
+
+    btn.classList =
+      "btn btn-outline btn-xs sm:btn-sm md:btn-md lg:btn-lg px-16 border-gray-300 hover:bg-[#0E7A811A] hover:border-primary hover:text-black";
+
     btn.innerHTML = `<img src=${pet?.category_icon} width="20px md:30px" alt=""> ${pet.category}`;
+    btn.setAttribute("onclick", `categoryPets('${pet.category}')`);
+    btn.setAttribute("id", `btn-${pet.category}`);
+
     categoriesContainer.append(btn);
   });
+};
+
+const categoryPets = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      showPets(data.data);
+    });
 };
 
 categories();
@@ -39,40 +40,63 @@ const petsData = () => {
 };
 
 const showPets = (data) => {
-  const petContainer = document.getElementById("pet-container");
-  data.forEach((pet) => {
-    const item = document.createElement("div");
-    item.classList = "card card-compact bg-base-100 w-96 shadow-xl border pt-5";
-    item.innerHTML = `
-          <figure >
-              <img
-              class="rounded-lg"
-                src=${pet?.image}
-                alt="Shoes" />
-            </figure>
-            <div class="card-body text-gray-500">
-              <h2 class="card-title text-black">${
-                pet?.pet_name ? pet?.pet_name : "Not Available"
-              }</h2>
-              <p><i class="fa-brands fa-squarespace"></i> Breed: ${
-                pet?.breed ? pet?.breed : "Not Available"
-              }</}</p>
-              <p><i class="fa-solid fa-calendar-days"></i> Birth: ${
-                pet?.date_of_birth ? pet?.date_of_birth : "Not Available"
-              }</p>
-              <p><i class="fa-solid fa-mercury"></i> Gender: ${
-                pet?.gender ? pet?.gender : "Not Available"
-              }</p>
-              <p><i class="fa-solid fa-mercury"></i> Price : ${
-                pet?.price ? pet?.price : "Not Available"
-              }</p>
-              <div class="flex justify-between border-t-2 py-3 items-center">
-                <button class="btn btn-sm text-primary text-xl px-8"><i class="fa-regular fa-thumbs-up"></i></button>
-                <button class="btn btn-sm text-primary text-lg">Adopt</button>
-                <button class="btn btn-sm text-primary text-lg">Details</button>
-              </div>
-        `;
-    petContainer.append(item);
-  });
+    const petContainer = document.getElementById("pet-container");
+
+    if(data.length===0){
+        petContainer.classList.remove('grid')
+        petContainer.classList.add('w-full')
+      
+        petContainer.innerHTML=`
+       <div class="w-full flex flex-col justify-center text-center items-center bg-[#13131308] rounded-lg p-10">
+        <img src='../images/error.webp' width="400px"/>
+        <h1 class='text-4xl font-bold my-4' >No Information Available</h1>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at </br>
+its layout. The point of using Lorem Ipsum is that it has a.</p>
+       </div>
+        `
+        
+
+    }
+    else{
+        petContainer.innerHTML = "";
+        petContainer.classList.add('grid')
+        petContainer.classList.add('w-full')
+        data.forEach((pet) => {
+          const item = document.createElement("div");
+          item.classList = "card card-compact bg-base-100  shadow-xl border pt-5";
+          item.innerHTML = `
+                <figure >
+                    <img
+                    class="rounded-lg"
+                      src=${pet?.image}
+                      alt="Shoes" />
+                  </figure>
+                  <div class="card-body text-gray-500">
+                    <h2 class="card-title text-black">${
+                      pet?.pet_name ? pet?.pet_name : "Not Available"
+                    }</h2>
+                    <p><i class="fa-brands fa-squarespace"></i> Breed: ${
+                      pet?.breed ? pet?.breed : "Not Available"
+                    }</}</p>
+                    <p><i class="fa-solid fa-calendar-days"></i> Birth: ${
+                      pet?.date_of_birth ? pet?.date_of_birth : "Not Available"
+                    }</p>
+                    <p><i class="fa-solid fa-mercury"></i> Gender: ${
+                      pet?.gender ? pet?.gender : "Not Available"
+                    }</p>
+                    <p><i class="fa-solid fa-mercury"></i> Price : ${
+                      pet?.price ? pet?.price : "Not Available"
+                    }</p>
+                    <div class="flex justify-between border-t-2 py-3 items-center">
+                      <button class="btn btn-sm text-primary text-xl px-8"><i class="fa-regular fa-thumbs-up"></i></button>
+                      <button class="btn btn-sm text-primary text-lg">Adopt</button>
+                      <button class="btn btn-sm text-primary text-lg">Details</button>
+                    </div>
+              `;
+          petContainer.append(item);
+        });
+
+    }
+
 };
 petsData();
